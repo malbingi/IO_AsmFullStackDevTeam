3c45d9892caee32b34a1db70290d9cb5393f487f
fiszmon <skrzypalke@gmail.com>
2019-11-26 23:45:36 +0100

    Hist 1 - wczytywanie pliku
    Klasa CodeReader do wczytywania importów z pliku. Przykład użycia w main.py

---
+++ hist_1/hist1/main.py
@@ -0,0 +1,32 @@
+from CodeReaderService import CodeReader
+import os
+
+
+def main():
+    path = ".\\resources"
+    files = []
+    for r, d, f in os.walk(path):
+        for file in f:
+            if '.py' in file:
+                files.append(os.path.join(r, file))
+
+    for f in files:
+        cs = CodeReader(f)
+        cs.load_file_data()
+
+        print(cs.get_name())
+        if len(cs.get_imports()) == 0:
+            print("No imports\n\n")
+
+        for im in cs.get_imports():
+            print_str = "Import: " + im.get_source_name() + "\n"
+            i = 1
+            for f_r in im.files_ref:
+                print_str += str(i) + ". " + f_r.reference_name\
+                             + " calls: " + str(f_r.call_count) + '\n'
+                i += 1
+            print(print_str)
+
+
+if __name__ == "__main__":
+    main()
