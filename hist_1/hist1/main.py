d95b3bcc123298ba73d81140ccafc5ff07f4be54
PawelDziwura <paweldziwura1881997@gmail.com>
2020-01-08 23:52:18 +0100

    Realizacja diagramu dla historyjek 1 - 2

--- hist_1/hist1/main.py
+++ hist_1/hist1/main.py
@@ -1,10 +1,13 @@
 from packages.CodeReaderService import CodeReader, reload_counters
 import os
+import graphviz
 
 
 def main():
     path = ".\\resources"
     files = []
+    dict = {}
+
     for r, d, f in os.walk(path):
         for file in f:
             if '.py' in file:
@@ -19,17 +22,31 @@ def main():
         cs.check_references()
 
         print("========= File: " + cs.get_name() + " ================")
+        elements = []
         if len(cs.get_imports()) == 0:
             print("No imports\n\n")
 
         for im in cs.get_imports():
             print_str = "Import: " + im.get_source_name() + "\n"
             i = 1
+            total = 0
             for f_r in im.files_ref:
-                print_str += str(i) + ". " + f_r.reference_name\
+                print_str += str(i) + ". " + f_r.reference_name \
                              + " calls: " + str(f_r.call_count) + '\n'
+                total += f_r.call_count
                 i += 1
+            if im.is_local:
+                elements.append([im.get_source_name(), total])
             print(print_str)
+
+        dict[cs.get_name()] = elements
+
+    dot = graphviz.Digraph(comment='References graph')
+    for item in dict:
+        dot.node(item, item)
+        for child in dict[item]:
+            dot.edge(item, child[0], str(child[1]))
+    dot.render('test-output/round-table.gv', view=True)
 
 
 if __name__ == "__main__":
--- hist_2/hist2/main.py
+++ hist_2/hist2/main.py
@@ -1,10 +1,12 @@
 from packages.CodeReaderService import CodeReader, reload_counters
 import os
-
+import graphviz
 
 def main():
     path = "..\\..\\hist_1\\hist1\\resources"
     files = []
+    dict = {}
+
     for r, d, f in os.walk(path):
         for file in f:
             if '.py' in file:
@@ -17,26 +19,24 @@ def main():
     reload_counters()
     for cs in code_readers:
         cs.check_references()
-        # print("========= File: " + cs.get_name() + " ================")
-        # if len(cs.get_imports()) == 0:
-        #     print("No imports\n\n")
-
-        # for im in cs.get_imports():
-        #     print_str = "Import: " + im.get_source_name() + "\n"
-        #     i = 1
-        #     for f_r in im.files_ref:
-        #         print_str += str(i) + ". " + f_r.reference_name\
-        #                      + " calls: " + str(f_r.call_count) + '\n'
-        #         i += 1
-        #     print(print_str)
 
     for m in CodeReader.Methods.values():
         if not m.is_class_method:
             print_str = "Name: " + str(m.index) + ' - ' + m.name + "\n[file, called] - [" \
                         + str(CodeReader.CodeReaders.get(m.file_index).filename) + ', ' + str(m.call_count) + "]\n"
+            elements = []
             for k, v in m.call_reference.items():
                 print_str += "\tMethod: " + str(k) + " - " + str(CodeReader.Methods[k].name) + " was called - " + str(v.call_count) + "\n"
+                elements.append([CodeReader.Methods[k].name, v.call_count])
+            dict[m.name] = elements
             print(print_str)
+
+    dot = graphviz.Digraph(comment='References graph')
+    for item in dict:
+        dot.node(item, item)
+        for child in dict[item]:
+            dot.edge(item, child[0], str(child[1]))
+    dot.render('test-output/round-table.gv', view=True)
 
 
 if __name__ == "__main__":
