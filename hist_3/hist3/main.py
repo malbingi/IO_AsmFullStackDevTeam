aa84e9b720f02fcb149c089015d4f3316f758e8a
PawelDziwura <paweldziwura1881997@gmail.com>
2020-01-10 16:24:05 +0100

    Realizacja diagramu do historyjki 3

--- hist_3/hist3/main.py
+++ hist_3/hist3/main.py
@@ -1,10 +1,12 @@
 from packages.CodeReaderService import CodeReader, get_method_from_class_key, reload_counters
 import os
-
+import graphviz
 
 def main():
     path = "..\\..\\hist_1\\hist1\\resources"
     files = []
+    dict = {}
+    method_ref_dict = {}
     for r, d, f in os.walk(path):
         for file in f:
             if '.py' in file:
@@ -30,6 +32,7 @@ def main():
                     + CodeReader.CodeReaders.get(c.file_index).filename\
                     + ", " + str(c.init_count) + "]\n"\
                     + "GLOBAL CALLS:\n"
+
         for k, v in c.call_reference.items():
             if type(k) is not int:
                 key = get_method_from_class_key(k)
@@ -40,10 +43,37 @@ def main():
             else:
                 print_str += "\tMethod: GLOBAL"
             print_str += " was called - " + str(v.call_count) + "\n"
+
         print_str += "\nLOCAL METHODS:\n"
+
+        class_content = []
         for m in c.methods.values():
+            class_content.append([m.name, m.call_count])
+            references = []
+            for k in m.call_reference.keys():
+
+                if type(k) is not int:
+                    if k.find("-") > -1:
+                        k = get_method_from_class_key(k)
+                references.append([CodeReader.Methods[k].name, CodeReader.Methods[k].call_count])
+
+            method_ref_dict[m.name] = references
             print_str += print_method(m)
         print(print_str)
+
+        dict[c.name] = class_content
+
+    dot = graphviz.Digraph(comment='References graph')
+    for item in dict:
+        dot.node(item, item)
+        for child in dict[item]:
+            dot.edge(item, child[0], str(child[1]))
+
+    for item_ in method_ref_dict:
+        dot.node(item_, item_)
+        for child_ in method_ref_dict[item_]:
+            dot.edge(item_, child_[0], str(child_[1]))
+    dot.render('test-output/round-table.gv', view=True)
 
 
 def print_method(m):
